aa79257a499d223814d4405cece05bf8f1cc664f
alanayee <61395196+alanayee@users.noreply.github.com>
2020-04-19 16:36:02 -0700

    Add files via upload

--- restaurant.java
+++ restaurant.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.lang.Math;
 public class restaurant {
     public static void main (final String[] args) { 
         //Scanner inputs = new Scanner(Sysetm.in);
@@ -22,58 +23,76 @@ public class restaurant {
         If statements: used to add price to total price and add strings to the array (to add on final receipt)
        */
         ArrayList<String> orderArray = new ArrayList<String>();
-        System.out.println("Enter the number of the item you would like to order");
-        double totalPrice = 0.0;
+        System.out.println("Enter the number of the item(s) you would like to order");
+        double subTotal = 0.0;
         for(int i=0; i<size; i++) {
             Scanner item = new Scanner(System.in);
             String foodItem = item.next();
             int food = Integer.parseInt(foodItem);
             if (food == 1){
-                totalPrice += californiaRolls;
+                subTotal += californiaRolls;
                 String str = "California Rolls";
                 orderArray.add(str);
             }
             if (food == 2){
-                totalPrice += ramen;
+                subTotal += ramen;
                 String str = "Ramen";
                 orderArray.add(str);
             }
             if (food == 3){
-                totalPrice += quesadilla;
+                subTotal += quesadilla;
                 String str = "Quesadilla";
                 orderArray.add(str);
             }
             if (food == 4){
-                totalPrice += bentoBox;
+                subTotal += bentoBox;
                 String str = "Bento Box";
                 orderArray.add(str);
             }
             if (food == 5){
-                totalPrice += pasta;
+                subTotal += pasta;
                 String str = "Pasta";
                 orderArray.add(str);
             }
             if (food ==6){
-                totalPrice += milkTea;
+                subTotal += milkTea;
                 String str = "Milk Tea";
                 orderArray.add(str);
             }
             if (food == 7){
-                totalPrice += icedLatte;
+                subTotal += icedLatte;
                 String str = "Iced Latte";
                 orderArray.add(str);
             }
             if (food == 8){
-                totalPrice += matchaTea;
+                subTotal += matchaTea;
                 String str = "Matcha Latte";
                 orderArray.add(str);
             }
+            if (food < 1 || food > 8){
+                System.out.println("Invalid order number. Please restart order and type in valid number(s).");
+                System.exit(0);
+            }
         }
+        //double roundOff = (double) Math.round(a * 100) / 100;
+
+        double totalPrice = subTotal;
+        System.out.println("How much tip would you like to give? Input 10 for 10% and 15 for 15% ");
+        Scanner tip = new Scanner(System.in);
+        int waiter_tip= tip.nextInt();
+        if (waiter_tip == 10){
+            totalPrice = totalPrice * 1.10;
+        }
+        if (waiter_tip == 15){
+            totalPrice = totalPrice *1.15;
+        }
+        subTotal = (double) Math.round(subTotal * 100) / 100;
+        totalPrice = (double) Math.round(totalPrice * 100) / 100;
         System.out.println("Your order is: ");
         for (int i = 0; i<orderArray.size(); i++){
             System.out.println(orderArray.get(i));
         }
-        System.out.println("Your price is $" + totalPrice);
+        System.out.println("Your subtotal is $" + subTotal + " Your total price is $" + totalPrice);
         
     }
     
